2cd2a293951c11a8aca51f07fd80d5fe78fca798
Liang Wang <liang.wang.84@gmail.com>
2025-10-28 11:31:39 +0800

    add test

--- policy_clustering.py
+++ policy_clustering.py
@@ -47,10 +47,10 @@ def policy_standardize_monthly(input_policy_list: list, standard_policy_list: li
     ["标准化政策名称", "标准化政策名称", ..., "标准化政策名称"]
     """
     standard_policy_embed_dir = {}
-    for std_policy in standard_policy_list:
-        std_policy_name = list(std_policy.keys())[0]
-        std_policy_text = list(std_policy.values())[0]
-        std_policy_text = f"{std_policy_name} {std_policy_text}"
+    for std_policy_text in standard_policy_list:
+        # std_policy_name = list(std_policy.keys())[0]
+        # std_policy_text = list(std_policy.values())[0]
+        # std_policy_text = f"{std_policy_name} {std_policy_text}"
         std_policy_emb = ep_model.embed_text(std_policy_text)
         standard_policy_embed_dir[std_policy_name] = std_policy_emb
 
@@ -65,23 +65,53 @@ def policy_standardize_monthly(input_policy_list: list, standard_policy_list: li
     return ret_list
 
 if __name__ == "__main__":
-    input_policy_list = [
-        {"欧盟GPS/Galileo开放信号政策": "欧盟宣布民用Galileo信号免费并鼓励多星座接收，提升民用定位精度；osmdroid后续版本增加对GnssStatus.Callback等多星座接口的封装，使离线定位更准确。"},
-        {"欧盟GDPR生效": "开发者需对地图数据收集做合规审计，部分企业因担心聚合插件可能缓存用户坐标而弃用第三方扩展，Issues中出现银行与医疗客户移除该库的反馈"},
-        {"中国工信部 164 号文 IPv6 改造": "要求新上架 App 必须支持 IPv6-Only 网络。YTKNetwork 2.6.0 默认启用 NSURLSession 的 IPv6 解析，并给出“IPv6 失败-降级-IPv4”示例配置，帮助开发者快速过检。"},
-        {"Google UMP + GDPR 合规框架": "Google要求2021Q2起所有欧洲流量必须使用IAB TCF 2.0合规的UMP表单；AdColony SDK 4.6迅速内置Google UMP适配器并开源TCF String解析，帮助开发者一次集成即可同时满足AdColony+AdMob的GDPR合规，提高SDK采用率。"},
-        {"欧盟GDPR对开源项目合规成本提升": "GDPR要求开源库若收集IP、邮箱需披露数据流向；DKChainableAnimationKit Demo内置Firebase埋点统计崩溃，被欧洲开发者质疑合规，主导者最终移除统计代码并发布无追踪版本，增加维护负担。"},
-        {"工信部《推动5G+工业互联网512工程通知》": "明确鼓励5G+工业互联网场景采用国产实时通信中间件。腾讯云随后将TIMSDK的5G QoS接口、工厂局域网穿透模块开源，获得江苏、广东多家工业SaaS订单。"},
-        {"工信部要求 App 备案并规范权限调用": "备案制推动国内 App 瘦身、减少三方 SDK；ZoomImage 零网络权限、零依赖，契合合规诉求，成为对安全敏感厂商的首选。"},
-        {"宽带中国战略及IPv6升级": "工信部要求新建智能终端默认支持IPv6，node-android在0.4.0版本立即加入IPv6-UDP打洞，使其在政府智慧路灯、抄表项目中中标。"},
+    month_list = ["2010_08"]
+    input_policy_list = []
+    input_policy_records = []
+    for month_str in month_list:
+        with open(f"../policy_classification_data/policy_std/{month_str}_input.json", "r", encoding="utf-8") as fd:
+            for line in fd:
+                line = line.strip()
+                if line:
+                    input_policy = json.loads(line)
+                    input_policy_records.append(input_policy)
+                    input_policy_list.append({input_policy['event']: input_policy['policy_content']['政策内容和影响']})
+        standard_policy_list = []
+        standard_policy_type = {}
+        with open(f"../policy_classification_data/policy_std/{month_str}_events.json", "r", encoding="utf-8") as fd:
+            data = json.load(fd)
+            for type in ["政府政策", "平台举措", "其它事件"]:
+                if len(data[type]) == 0:
+                    continue
+                for item in data[type]:
+                    standard_policy_list.append(item)
+                    standard_policy_type[item] = type
+        ret_list = policy_standardize_monthly(input_policy_list, standard_policy_list)
+        print(ret_list)
+        with open(f"../policy_classification_data/policy_std/{month_str}_output.json", "w", encoding="utf-8") as fd:
+            for idx, std_policy_name in enumerate(ret_list):
+                input_policy_records[idx]['std_event'] = std_policy_name
+                input_policy_records[idx]['std_event_type'] = standard_policy_type[std_policy_name]
+                fd.write(json.dumps(input_policy_records[idx], ensure_ascii=False) + "\n")
+
+
+
+            
+    # input_policy_list = [
+    #     {"欧盟GPS/Galileo开放信号政策": "欧盟宣布民用Galileo信号免费并鼓励多星座接收，提升民用定位精度；osmdroid后续版本增加对GnssStatus.Callback等多星座接口的封装，使离线定位更准确。"},
+    #     {"欧盟GDPR生效": "开发者需对地图数据收集做合规审计，部分企业因担心聚合插件可能缓存用户坐标而弃用第三方扩展，Issues中出现银行与医疗客户移除该库的反馈"},
+    #     {"中国工信部 164 号文 IPv6 改造": "要求新上架 App 必须支持 IPv6-Only 网络。YTKNetwork 2.6.0 默认启用 NSURLSession 的 IPv6 解析，并给出“IPv6 失败-降级-IPv4”示例配置，帮助开发者快速过检。"},
+    #     {"Google UMP + GDPR 合规框架": "Google要求2021Q2起所有欧洲流量必须使用IAB TCF 2.0合规的UMP表单；AdColony SDK 4.6迅速内置Google UMP适配器并开源TCF String解析，帮助开发者一次集成即可同时满足AdColony+AdMob的GDPR合规，提高SDK采用率。"},
+    #     {"欧盟GDPR对开源项目合规成本提升": "GDPR要求开源库若收集IP、邮箱需披露数据流向；DKChainableAnimationKit Demo内置Firebase埋点统计崩溃，被欧洲开发者质疑合规，主导者最终移除统计代码并发布无追踪版本，增加维护负担。"},
+    #     {"工信部《推动5G+工业互联网512工程通知》": "明确鼓励5G+工业互联网场景采用国产实时通信中间件。腾讯云随后将TIMSDK的5G QoS接口、工厂局域网穿透模块开源，获得江苏、广东多家工业SaaS订单。"},
+    #     {"工信部要求 App 备案并规范权限调用": "备案制推动国内 App 瘦身、减少三方 SDK；ZoomImage 零网络权限、零依赖，契合合规诉求，成为对安全敏感厂商的首选。"},
+    #     {"宽带中国战略及IPv6升级": "工信部要求新建智能终端默认支持IPv6，node-android在0.4.0版本立即加入IPv6-UDP打洞，使其在政府智慧路灯、抄表项目中中标。"},
         
-    ]
-    standard_policy_list = [
-        {"欧盟GDPR政策": "GDPR要求开源库若收集IP、邮箱、位置等需披露数据流向"},
-        {"Galileo开放信号": "Galileo开放民用卫星信号"},
-        {"工信部《推动5G+工业互联网512工程通知》": "5G+工业互联网512工程通知要求5G基站支持10000个并发用户。"},
-        {"工信部 164 号文 IPv6 改造": "要求新上架 App 必须支持 IPv6 网络"},
-        {"工信部要求 App 备案并规范权限调用": "要求移动APP必须备案"}
-    ]
-    ret_list = policy_standardize_monthly(input_policy_list, standard_policy_list)
-    print(ret_list)
+    # ]
+    # standard_policy_list = [
+    #     {"欧盟GDPR政策": "GDPR要求开源库若收集IP、邮箱、位置等需披露数据流向"},
+    #     {"Galileo开放信号": "Galileo开放民用卫星信号"},
+    #     {"工信部《推动5G+工业互联网512工程通知》": "5G+工业互联网512工程通知要求5G基站支持10000个并发用户。"},
+    #     {"工信部 164 号文 IPv6 改造": "要求新上架 App 必须支持 IPv6 网络"},
+    #     {"工信部要求 App 备案并规范权限调用": "要求移动APP必须备案"}
+    # ]
